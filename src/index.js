aa9cb44b6df0a000e3828732496395bb2b05d3d0
TCristianB <tcristianb01@gmail.com>
2021-01-02 22:45:55 -0300

    Terminando el proyecto.

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const inputForm = document.querySelectorAll('.input-form')
+const iconError = document.querySelectorAll('.icon-error')
+const textError = document.querySelectorAll('.text-error')
+const submitButton = document.getElementById('input-button')
+
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    for (let i = 0; i < inputForm.length; i++) {
+        if(!inputForm[i].value) {
+            iconError[i].classList.add('visible')
+            textError[i].classList.add('visible')
+        } else {
+            iconError[i].classList.remove('visible')
+            textError[i].classList.remove('visible')
+        }
+    }
+})
+
